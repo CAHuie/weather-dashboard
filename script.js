6d0c4cf82b74855b3b1edf028e1dad858c14d892
chris <christopher.huie3@gmail.com>
2021-12-09 12:56:03 -0800

    created fetch for both api links

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var apiKey = "ec364f59ee730bbc6cecc64065bda738"
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
-var apiLink = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+
 var searchButton = document.querySelector("#search-button");
 function handleSearchButtonClick() {
     var searchInputVal = document.querySelector("#search-input").value;
     console.log(searchInputVal);
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInputVal}&appid=${apiKey}`;
+    fetch(apiUrl).then(function(res){
+        return res.json()
+    }).then(function(data){
+        console.log(data)
+        var apiLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`
+        return fetch(apiLink)
+    })
+    .then(function(res){
+        return res.json()
+    }).then(function(data){
+        console.log(data)
+    })
 }
 searchButton.addEventListener("click", handleSearchButtonClick)
